823d5d932dccacfd1c4cfc4850aa9a22271c8dcd
harrychou <harrychou@gmail.com>
2011-09-09 15:22:43 -0400

    Simple style

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 PersonalSite::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-	get "pages/about"
 
+	match '/', :to => "pages#home"
+	match 'about', :to => "pages#about"
+	match 'contact', :to => "pages#contact"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
